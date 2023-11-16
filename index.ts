b4c091392618ba74b39e574d82aa29a178b09e16
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-11-16 21:53:14 +0200

    build: install puppeteer & create demo script

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log('Hello via Bun!')
+import puppeteer from 'puppeteer'
+
+(async () => {
+  const browser = await puppeteer.launch()
+
+  const page = await browser.newPage()
+
+  await page.goto('https://developer.chrome.com')
+
+  await browser.close()
+})()
+
+console.info('Hello via Bun!')
