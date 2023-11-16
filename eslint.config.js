8ab5c89537865f74ca65d8730bd95b709879235f
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-11-17 00:21:51 +0200

    style: update eslint to remove unused imports

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,6 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   rules: {
     'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+    'unused-imports/no-unused-imports': 'error',
   },
 })
